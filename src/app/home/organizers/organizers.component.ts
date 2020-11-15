db360fcc75893ffc54069c5aa285c4612cfa853c
devferx <ferkki@hotmail.com>
2020-11-15 19:33:51 -0400

    import unnecessary removed

--- src/app/home/organizers/organizers.component.ts
+++ src/app/home/organizers/organizers.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import organizers from 'src/assets/data/organizers';
 
-import { Person } from '../../core/models/organizer.model';
-
 @Component({
   selector: 'ngs-organizers',
   templateUrl: './organizers.component.html',
