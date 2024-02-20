02f472d4cdad6cd949155994e0f50b0533eb32d7
Felix Alejandro Zelaya Orellana <fazogato@gmail.com>
2024-02-19 22:31:17 -0400

    update image sizing and refactor styling

--- src/assets/data/mentors.ts
+++ src/assets/data/mentors.ts
@@ -4,7 +4,7 @@ const mentors: Person[] = [
   {
     name: 'Mauricio Arce Torrez',
     shortName: 'Mauricio Arce',
-    imageSrc: 'assets/img/mentors/mauricio-arce.jpeg',
+    imageSrc: 'assets/img/mentors/mauricio-arce.jpg',
     bio: `Mauri es ingeniero de sistemas. Actualmente trabaja a tiempo completo como technical lead en desarrollo web para proyectos del exterior, además de trabajar en proyectos pequeños de front-end en sus ratos libres. Es un entusiasta de las comunidades tecnológicas y de desarrollo, fungiendo en estos momentos como lead organizer de Angular Bolivia y GDG Cochabamba.`,
     socialLinks: [
       {
@@ -16,7 +16,7 @@ const mentors: Person[] = [
   {
     name: 'Luis Aviles',
     shortName: 'Luis Aviles',
-    imageSrc: 'assets/img/mentors/luis-aviles.jpeg',
+    imageSrc: 'assets/img/mentors/luis-aviles.jpg',
     bio: `Luis es Ingeniero de Sistemas y ha trabajado en el área de Ingeniería de Software para compañías norteamericanas de impacto global y startups desde 2011. Ha participado como expositor en múltiples conferencias internacionales de tecnología y es autor de artículos técnicos sobre desarrollo web y cursos en linea. Debido a sus contribuciones, ha sido reconocido por Google, Microsoft y Cloudinary. Cuando no está programando, le gusta viajar, explorar el espacio y hacer Astrofotografía.`,
     socialLinks: [
       {
@@ -28,7 +28,7 @@ const mentors: Person[] = [
   {
     name: 'Rene Cardozo',
     shortName: 'Rene Cardozo',
-    imageSrc: 'assets/img/mentors/rene-cardozo.jpeg',
+    imageSrc: 'assets/img/mentors/rene-cardozo.jpg',
     bio: `Desarrollador Full Stack calificado familiarizado con una amplia gama de utilidades y lenguajes de programación. Conocedor de los requisitos de desarrollo backend y frontend. Capaz de manejar cualquier parte del proceso con facilidad. Trabajador de equipo colaborativo con excelentes habilidades técnicas que ofrece 9 años de experiencia relacionada. Antes participo activamente en los meetups de la comunida angular`,
     socialLinks: [
       {
@@ -40,7 +40,7 @@ const mentors: Person[] = [
   {
     name: 'Vania Marisela Catorceno Carballo',
     shortName: 'Vania Catorceno',
-    imageSrc: 'assets/img/mentors/vania-catorceno.jpeg',
+    imageSrc: 'assets/img/mentors/vania-catorceno.jpg',
     bio: `Ingeniera de Sistemas. Actualmente trabaja en Jalasoft como desarrolladora de software con el rol de Lead técnico en el área de Frontend. Con mucho entusiasmo por compartir conocimiento y aprender mucho más. Participa activamente en comunidades como Organizer de GDG Cochabamba, WTM Ambassador Cochabamba, Co-Founder de GP4Tech (Girl Power For Technology), Organizer WGJ (Women Game Jam) Bolivia. Trainer en bootcamp en Jala University.`,
     socialLinks: [
       {
@@ -52,7 +52,7 @@ const mentors: Person[] = [
   {
     name: 'Ronald Humberto Chipana Wariste',
     shortName: 'Ronald Chipana',
-    imageSrc: 'assets/img/mentors/ronald-chipana.png',
+    imageSrc: 'assets/img/mentors/ronald-chipana.jpg',
     bio: `Es ingeniero de software de profesión y cuenta con experiencia en el área de desarrollo de aplicaciones con herramientas de google, desarrollo de videojuegos para la educación y es docente de programación. Le apasiona la enseñanza de la tecnología razón por la cual  fundó la Academia TechKids donde promueve la educación STEAM desde temprana edad; es una persona muy dinámica y le encanta aprender, razón por la cual forma parte de distintas comunidades como GDG Cochabamba, Women Techmakers Cochabamba y Angular Bolivia.`,
     socialLinks: [
       {
@@ -64,7 +64,7 @@ const mentors: Person[] = [
   {
     name: 'Angel Christian Flores Lazarte',
     shortName: 'Angel Flores',
-    imageSrc: 'assets/img/mentors/angel-flores.jpeg',
+    imageSrc: 'assets/img/mentors/angel-flores.jpg',
     bio: `Estudiante de ing de sistemas en la UMSS, trabaja como desarrolador fullstack Jr en Digital Harbor, apasionado por aprender cosas nuevas y enseñar siempre que puede, miembro activo de GDG, SCESI y angular Bolivia.`,
     socialLinks: [
       {
@@ -76,7 +76,7 @@ const mentors: Person[] = [
   {
     name: 'Maria Griselda Garcia Flores',
     shortName: 'Griselda Garcia',
-    imageSrc: 'assets/img/mentors/griselda-garcia.jpeg',
+    imageSrc: 'assets/img/mentors/griselda-garcia.jpg',
     bio: `Griselda es ingeniera de sistemas que actualmente trabaja como desarrolladora de software en Jalasoft desde hace más de 5 años. Es apasionada por la educación, razón por la cual es instructora en el bootcamp de Jala University a nivel latinoamericano, enseñando tecnologías front-end y Angular. Es embajadora de Women Techmakers y la organizadora principal de las comunidades GDG Cochabamba y Angular Bolivia. Tiene un posgrado en Gestión de Tecnologías de la Información, el cual ha sido muy útil para liderar equipos y crear proyectos. Cuando no está programando, Griselda juega con sus gatitos, disfruta viendo películas y series, viaja, pasa el tiempo con sus amigos y hace CrossFit.`,
     socialLinks: [
       {
@@ -88,7 +88,7 @@ const mentors: Person[] = [
   {
     name: 'Dámaris Gosálvez',
     shortName: 'Dámaris Gosálvez',
-    imageSrc: 'assets/img/mentors/damaris-gosalvez.jpeg',
+    imageSrc: 'assets/img/mentors/damaris-gosalvez.jpg',
     bio: `Damaris comparte sus conocimientos a través de la docencia, brindando a los estudiantes la oportunidad de aprender en el campo de la informática, participa activamente de las comunidades Mujeres que Inspiran y Python Cbba.`,
     socialLinks: [
       {
@@ -100,7 +100,7 @@ const mentors: Person[] = [
   {
     name: 'Andrea Megan Lafuente Rocha',
     shortName: 'Andrea Lafuente',
-    imageSrc: 'assets/img/mentors/andrea-lafuente.jpeg',
+    imageSrc: 'assets/img/mentors/andrea-lafuente.jpg',
     bio: `Andrea Megan, una desarrolladora web con experiencia en Angular, está inmersa en un proyecto empresarial. Apasionada por compartir conocimientos, busca constantemente mejorar y colaborar con otros en su campo.`,
     socialLinks: [
       {
@@ -112,7 +112,7 @@ const mentors: Person[] = [
   {
     name: 'Sara Camila Laura Manzaneda',
     shortName: 'Sara Laura',
-    imageSrc: 'assets/img/mentors/sara-laura.jpeg',
+    imageSrc: 'assets/img/mentors/sara-laura.jpg',
     bio: `Camila es una ingeniera a la que le encanta resolver problemas, desde los más simples hasta los más técnicos. Además, es una apasionada por compartir su conocimiento y experiencia con la comunidad de developers. Tiene más de dos años de experiencia como Ingeniera de Software (mid) trabajando en una Fintech como Libelula SRL, hasta en empresas de outsourcing como Jalasoft y actualmente UnoSquare. Se considera una full Stack developer,  manejando el stack de .NET y ANGULAR, sin embargo tiene una inclinación algo mas profunda por el lado de frontend. Por otro lado, tiene experiencia en mentorias al haber impartido cursos de inteligencia artificial, procesamiento de imágenes medicas en un instituto llamado Machine Learning y brindando ayudantías en  ciencias básicas como calculo III, ecuaciones diferencias y fisicoquímica. Ha participado como coordinadora de la ITEC en la UCB y ha ganado en el segundo lugar en la ultima Hackaton de Ethereum realizada en la UCB. Como profesional, uno de sus más grandes deseos es implantar su conocimiento y experiencia en mujeres que deseen comenzar en el mundo del desarrollo web. Sabe que somos pocas las mujeres que se atreven a seguir este camino, sin embargo busca que muchas otras sigan sus sueños y se conviertan en grandes ingenieras de software.`,
     socialLinks: [
       {
@@ -124,7 +124,7 @@ const mentors: Person[] = [
   {
     name: 'Jacqueline Susan Mejía',
     shortName: 'Jacqueline Mejía',
-    imageSrc: 'assets/img/mentors/jacqueline-mejia.jpeg',
+    imageSrc: 'assets/img/mentors/jacqueline-mejia.jpg',
     bio: `Jacqueline, una apasionada del aprendizaje, especialmente en Machine Learning y Data Science, cuenta con una formación en Ingeniería de Sistemas e Ingeniería Informática, además de un diplomado en Data Science y Educación Superior basado en competencias. Jacqueline es Women Techmaker Ambassador y participa activamente en comunidades como GDG Cochabamba, WTM Cochabamba. Lidera la comunidad Ada in Dada, Mujeres TICś Cochabamba y es co-fundadora de Pyxtalks. Actualmente trabaja como Data Analyst en PyxJam, contribuye al éxito y desarrollo de la empresa aplicando su experiencia en tecnología y análisis de datos.`,
     socialLinks: [
       {
@@ -148,7 +148,7 @@ const mentors: Person[] = [
   {
     name: 'Nicolas Molina Monroy',
     shortName: 'Nicolas Molina',
-    imageSrc: 'assets/img/mentors/nicolas-molina.jpeg',
+    imageSrc: 'assets/img/mentors/nicolas-molina.jpg',
     bio: `"I’m Software Senior Developer and Teacher at Platzi. With more than 8 years of professional software development experience focusing in Web Development, Tech Management and skills for problem-solving. I’m passionate about sharing my knowledge with the community about Web Development and I do this through code contributions, videos, conferences, articles and, creating online content therefore I got recognized by Google like an GDE (Google Developer Expert)  in Angular and by Microsoft like an MVP (Most Value Professional). When I'm not coding share time with my son and my wife and I really like to travel across the world and meet other cultures.`,
     socialLinks: [
       {
@@ -160,7 +160,7 @@ const mentors: Person[] = [
   {
     name: 'David Morales Vega',
     shortName: 'David Morales',
-    imageSrc: 'assets/img/mentors/david-morales.png',
+    imageSrc: 'assets/img/mentors/david-morales.jpg',
     bio: `David Morales Vega, Ingeniero de Sistemas con más de cinco años de experiencia, se ha destacado en el desarrollo de aplicaciones web y móviles, trabajando con diversas tecnologías y marcos. Apasionado por el código y siempre entusiasmado por aprender nuevas tecnologías, David encuentra satisfacción en enseñar e impartir sus conocimientos a otros. Su dedicación hacia el desarrollo tecnológico se refleja tanto en su habilidad para adaptarse a diferentes tecnologías como en su compromiso con la educación y la transmisión de conocimientos.`,
     socialLinks: [
       {
@@ -172,7 +172,7 @@ const mentors: Person[] = [
   {
     name: 'Maria Fernanda Nuñez del Prado',
     shortName: 'Maria Nuñez',
-    imageSrc: 'assets/img/mentors/maria-nunez.jpeg',
+    imageSrc: 'assets/img/mentors/maria-nunez.jpg',
     bio: `Estoy muy emocionada por cómo se mueve últimamente el mundo de la tecnología, cada día es un nuevo día para aprender :) Desde el año pasado empecé a participar en la comunidad de Angular Bolivia y me encanta esta nueva experiencia!!`,
     socialLinks: [
       {
@@ -184,7 +184,7 @@ const mentors: Person[] = [
   {
     name: 'Fernando Quinteros Gutierrez ',
     shortName: 'Fernando Quinteros',
-    imageSrc: 'assets/img/mentors/fernando-quinteros.jpeg',
+    imageSrc: 'assets/img/mentors/fernando-quinteros.jpg',
     bio: `Fernando es Frontend Developer y Estudiante de ingeniería informática. Aprendió a programar a los 14 años encontrando inspiración en diferentes comunidades tech. Entre sus tecnologías favoritas se encuentran CSS, JavaScript y React. Pero le gusta aprender otras herramientas para el desarrollo de software en general.`,
     socialLinks: [
       {
@@ -196,7 +196,7 @@ const mentors: Person[] = [
   {
     name: 'Laura Risueño Arancibia',
     shortName: 'Laura Risueño',
-    imageSrc: 'assets/img/mentors/laura-risueno.jpeg',
+    imageSrc: 'assets/img/mentors/laura-risueno.jpg',
     bio: `Laura tiene más de 5 años de experiencia como ingeniera de software en diferentes empresas y actualmente trabaja en Assuresoft. Además de su sólida habilidad técnica, ha desarrollado habilidades fundamentales como la resolución de problemas, la mentoría, el aprendizaje constante, el liderazgo y la creatividad, entre otros. Es una entusiasta de la tecnología, apasionada por compartir sus conocimientos, lo que la ha llevado a ser embajadora de Women Techmakers (WTM) y a colaborar activamente en las comunidades GDG Cochabamba, WTM Cochabamba y Angular Bolivia.`,
     socialLinks: [
       {
@@ -208,7 +208,7 @@ const mentors: Person[] = [
   {
     name: 'Esther Romero Aguilar ',
     shortName: 'Esther Romero',
-    imageSrc: 'assets/img/mentors/esther-romero.jpeg',
+    imageSrc: 'assets/img/mentors/esther-romero.jpg',
     bio: `Esther es estudiante de Ingeniería Informática, miembro activo de GDG Cochabamba y Angular Bolivia, apasionada por las competencias de programación y de voluntariados a comunidades del mundo de la tecnología.`,
     socialLinks: [
       {
@@ -220,7 +220,7 @@ const mentors: Person[] = [
   {
     name: 'Cesar Gustavo Serrudo Huarachi',
     shortName: 'Gustavo Serrudo',
-    imageSrc: 'assets/img/mentors/gustavo-serrudo.jpeg',
+    imageSrc: 'assets/img/mentors/gustavo-serrudo.jpg',
     bio: `Desarrollador de software fullstack con experiencias de 9+ años, me gusta mucho la tecnologia, los juegos y aprender nuevas cosas.`,
     socialLinks: [
       {
@@ -232,7 +232,7 @@ const mentors: Person[] = [
   {
     name: 'Zulema Vicente',
     shortName: 'Zulema Vicente',
-    imageSrc: 'assets/img/mentors/zulema-vicente.png',
+    imageSrc: 'assets/img/mentors/zulema-vicente.jpg',
     bio: `Soy Zulema Vicente, de Cochabamba, Bolivia. Mi interés en la tecnología me llevó a estudiar Ingeniería Industrial y de Sistemas. Durante 4 años trabajé en el desarrollo de aplicaciones web y móviles. Luego, me especialicé en Diseño UX/UI en plataformas como Platzi y Coderhouse, aplicando mis habilidades en empresas como Pil Andina y Ganatech por más de 2 años. Soy parte de la comunidad de AngularBolivia.`,
     socialLinks: [
       {
@@ -244,7 +244,7 @@ const mentors: Person[] = [
   {
     name: 'Felix Alejandro Zelaya Orellana',
     shortName: 'Felix Zelaya',
-    imageSrc: 'assets/img/mentors/felix-zelaya.jpeg',
+    imageSrc: 'assets/img/mentors/felix-zelaya.jpg',
     bio: `Felix A. Zelaya O. más conocido como fazo, es Ingeniero Industrial, Ingeniero en Ciencias de la Computación y T.S. en Informática, apasionado por la tecnología, la música, el arte y la programación trabaja desde hace 3 años como Software Engineer en la empresa NiceCXOne`,
     socialLinks: [
       {
