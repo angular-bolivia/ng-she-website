0ae698795afc362dd72a0ecadcb2eb4acbb94694
Felix Alejandro Zelaya Orellana <fazogato@gmail.com>
2024-02-21 23:55:00 -0400

    8 - Actualizar la información de los organizadores

--- src/assets/data/organizers.ts
+++ src/assets/data/organizers.ts
@@ -4,11 +4,8 @@ const organizers: Person[] = [
   {
     name: 'Maria Griselda Garcia Flores',
     shortName: 'Griss Garcia',
-    imageSrc: 'assets/img/organizers/griss.jpg',
-    bio: `Ingeniera de sistemas con posgrado en gestión de tecnologías de la información. Desarrolladora web. Lead coorganizer de
-      las comunidades: Angular Bolivia, GP4Tech. Women techmaker ambassador, facilitadora #IAmRemarkable. Entusiasta por el
-      aprendizaje y la enseñanza. Le gusta ver series, viajar, participar en eventos de tecnología, enseñar programación y
-      manejar bicicleta.`,
+    imageSrc: 'assets/img/organizers/griselda-garcia.jpg',
+    bio: `Griselda es ingeniera de sistemas com más de 5 años trabajando en el desarrollo de aplicaciones web. Es apasionada por la educación, razón por la cual es instructora en el bootcamp de Jala University a nivel latinoamericano. Es embajadora de Women Techmakers y lead de las comunidades GDG Cochabamba y Angular Bolivia.Cuando no está programando, Griselda juega con sus gatitos, viaja y practica CrossFit.`,
     featured: true,
     socialLinks: [
       {
@@ -16,172 +13,200 @@ const organizers: Person[] = [
         link: 'https://twitter.com/grissgarcia074',
       },
       {
-        icon: 'fab fa-linkedin',
+        icon: 'fab fa-instagram',
         link:
-          'https://www.linkedin.com/in/maria-griselda-garcia-flores-b4518b184',
-      },
-      {
-        icon: 'fab fa-github',
-        link: 'https://github.com/mariagarciaflores',
-      },
-    ],
-  },
-  {
-    name: 'Lizzy Mendivil Bejarano',
-    shortName: 'Lizzy Mendivil',
-    imageSrc: 'assets/img/organizers/lizzy.jpg',
-    bio: `Frontend Developer en Jalasoft. Trainer en Fundación Jala. Lead en GP4Tech. Co-Organizer en Angular Bolivia. Le gusta
-      compartir sus conocimientos como speaker en tech events y a través de su blog. Cuando no está desarrollando, le gusta
-      crear y editar videos para su YouTube channel "La Vida de Li" o practicar mountain bike.`,
-    featured: true,
-    socialLinks: [
-      {
-        icon: 'fab fa-twitter',
-        link: 'https://twitter.com/lizzymendivil',
-      },
-      {
-        icon: 'fab fa-linkedin',
-        link: 'https://www.linkedin.com/in/lizzymendivil',
-      },
-      {
-        icon: 'fab fa-github',
-        link: 'https://github.com/lizzymendivil',
-      },
-    ],
-  },
-  {
-    name: 'Gabriela Torrico',
-    shortName: 'Gaby Torrico',
-    imageSrc: 'assets/img/organizers/gaby.jpg',
-    bio: `Una chica extrovertida, amante de las redes sociales y la tecnología, apasionada por el desarrollo web. Disfruta mucho
-      enseñar a los demás.`,
-    featured: true,
-    socialLinks: [
-      {
-        icon: 'fab fa-twitter',
-        link: 'https://twitter.com/Gaby_Torrico',
-      },
-      {
-        icon: 'fab fa-linkedin',
-        link: 'https://www.linkedin.com/in/gabriela-torrico-33925416a',
-      },
-    ],
-  },
-  {
-    name: 'Daniela Villalobos',
-    shortName: 'Dani Villalobos',
-    imageSrc: 'assets/img/organizers/dani.jpg',
-    bio: `Technical Writer, con 4 años de experiencia, amante de los números y apasionada por la enseñanza, docente de varias
-      materias de universidad, la mayoría relacionadas con números.`,
-  },
-  {
-    name: 'Gustavo Pacchi Villarroel',
-    shortName: 'Gustavo Pacchi',
-    imageSrc: 'assets/img/organizers/gus.jpg',
-    bio: `Entusiasta de los eventos de tecnología y el diseño, estudió ingeniería de sistemas, le gustan los libros, la
-      actividad física y la salud mental.`,
-    socialLinks: [
-      {
-        icon: 'fab fa-twitter',
-        link: 'https://twitter.com/guszUIUX',
-      },
-      {
-        icon: 'fab fa-linkedin',
-        link: 'https://www.linkedin.com/in/gustavo-pacchi-villarroel',
-      },
-      {
-        icon: 'fab fa-github',
-        link: 'https://github.com/gustavopacchi',
-      },
-    ],
+          'https://www.instagram.com/combigriss',
+      }
+    ]
   },
   {
     name: 'Mauricio Arce Torrez',
     shortName: 'Mauricio Arce',
-    imageSrc: 'assets/img/organizers/mauri.jpg',
-    bio: `Mauricio es un apasionado del desarrollo web y de participar en comunidades. Graduado de la carrera de Ingeniería de
-      Sistemas, actualmente trabaja como desarrollador de software en Jalasoft. Es parte del equipo co-organizador de la
-      comunidad Angular Bolivia, y miembro activo de las comunidades GP4Tech y Startup Weekend Cochabamba. Ha participado en
-      varias actividades llevadas a cabo por las comunidades ya nombradas, además de otras como ser GDG Cochabamba y Women
-      Techmakers Cochabamba, brindando su apoyo en tareas de desarrollo web y mentoría. En su tiempo libre, a Mauri le gusta
-      mucho hacer rutas de XC en bicicleta de montaña.`,
+    imageSrc: 'assets/img/organizers/mauricio-arce.jpg',
+    bio: `Mauri es ingeniero de sistemas. Actualmente trabaja a tiempo completo como technical lead en desarrollo web para proyectos del exterior, además de trabajar en proyectos pequeños de front-end en sus ratos libres. Es un entusiasta de las comunidades tecnológicas y de desarrollo, fungiendo en estos momentos como lead organizer de Angular Bolivia y GDG Cochabamba.`,
+    featured: true,
     socialLinks: [
       {
-        icon: 'fab fa-twitter',
-        link: 'https://twitter.com/combimauri',
-      },
-      {
-        icon: 'fab fa-linkedin',
-        link: 'https://www.linkedin.com/in/combimauri',
-      },
-      {
-        icon: 'fab fa-github',
-        link: 'https://github.com/combimauri',
-      },
-    ],
+        icon: 'fa fa-desktop',
+        link: 'https://combimauri.com',
+      }
+    ]
   },
   {
-    name: 'Brian Dennis Vega Hidalgo',
-    shortName: 'Brian Vega',
-    imageSrc: 'assets/img/organizers/brian.jpg',
-    bio: `Desarrollador mobile con más de 5 años de experiencia. Participante y organizador de las comunidades StartupWeekend y
-      Flutter Bolivia. Le encantan los videojuegos y los juegos de mesa.`,
+    name: 'Cristhian Rocha',
+    shortName: 'Cristhian Rocha',
+    imageSrc: 'assets/img/organizers/cristian-rocha.png',
+    bio: `Cristhian es un estudiante de Ing. en Sistemas en la UMSS apasionado por la tecnología, actualmente está iniciando su camino en el desarrollo mobile con Flutter y backend con Express, le gusta mucho participar activamente en comunidades de tecnología. Sus pasatiempos son consumir streaming y los videojuegos.`,
     socialLinks: [
       {
-        icon: 'fab fa-twitter',
-        link: 'https://twitter.com/BrianDVegaH',
+        icon: 'fab fa-instagram',
+        link: 'https://www.instagram.com/cristhian_rocha17/',
       },
       {
         icon: 'fab fa-linkedin',
-        link: 'https://www.linkedin.com/in/brian-dennis-vega-hidalgo',
+        link: 'https://www.linkedin.com/in/cristhian-rocha-ferrufino/',
       },
       {
         icon: 'fab fa-github',
-        link: 'https://github.com/DentVega',
-      },
-    ],
+        link: 'https://github.com/cristhian2020',
+      }
+    ]
   },
   {
-    name: 'Fernando Quinteros Gutierrez',
-    shortName: 'Fer Quinteros',
-    imageSrc: 'assets/img/organizers/fer.jpg',
-    bio: `Fernando es un estudiante del colegio San Agustín, entusiasmado por crear proyectos con tecnologías web y mobile. Le
-      gusta poder compartir su conocimiento por diferentes medios como ser su blog personal o dando charlas técnicas.`,
+    name: 'Felix Zelaya',
+    shortName: 'Felix Zelaya',
+    imageSrc: 'assets/img/organizers/felix-zelaya.jpg',
+    bio: `Felix, más conocido como fazo es Ing. Industrial, Ing. en Ciencias de la Computación y T.S. en Informática, con mas de 3 años de experiencia desarrollando aplicaciones web y mobile, apasionado por el aprendizaje continuo, el arte y la ciencia, actualmente trabaja en NiceCXOne, dedica su tiempo libre a la música, powerlifting y bicicleta.`,
     socialLinks: [
       {
-        icon: 'fab fa-twitter',
-        link: 'https://twitter.com/devferx',
-      },
-      {
         icon: 'fab fa-linkedin',
-        link: 'https://www.linkedin.com/in/devferx',
-      },
-      {
-        icon: 'fab fa-github',
-        link: 'https://github.com/devferx',
-      },
-    ],
+        link: 'https://www.linkedin.com/in/fazozela',
+      }
+    ]
   },
   {
-    name: 'Ivo Rojas',
-    shortName: 'Ivo Rojas',
-    imageSrc: 'assets/img/organizers/ivo.jpg',
-    bio: `Desarrollador de software, amante de la tecnología, naturaleza, artes visuales y el bienestar personal.`,
+    name: 'Melany Rocha Ferrufino',
+    shortName: 'Melany Rocha',
+    imageSrc: 'assets/img/organizers/melany-rocha.png',
+    bio: `Melany es una estudiante de Ingeniería Informática en la UMSS apacionada por la tecnología, está iniciando su camino en el desarrollo Web, le encanta el voluntariado en comunidades de tecnología colaborando en la organización de eventos, por lo cual pertence a GDG Cochabamba y Angular Bolivia como miembro activo, tiene como hobby's leer, cantar, gym, jugar con su gatita y perrita.`,
+    socialLinks: [
+      {
+        icon: 'fab fa-instagram',
+        link: 'https://www.instagram.com/melany.rocha.code?igsh=ZTU5MGRkOHVsdnFn',
+      }
+    ]
+  },
+  {
+    name: 'Gustavo Pacchi Villarroel',
+    shortName: 'Gustavo Pacchi',
+    imageSrc: 'assets/img/organizers/gustavo-pachi.png',
+    bio: `Gustavo es Product Designer con amplia experiencia en diseño de interfaces, de experiencias de usuario, desarrollo de marcas, diseño gráfico y diseño de animaciones. Entusiasta por la tecnología y los eventos, participa activamente en comunidades ejerciendo como líder, como diseñador y/o editando videos para promocionar eventos. Sus aficiones son trotar, nadar y pasar tiempo con sus perros.`,
+    featured: true,
     socialLinks: [
       {
         icon: 'fab fa-twitter',
-        link: 'https://twitter.com/ircube',
+        link: 'https://twitter.com/gusz_pv',
       },
       {
         icon: 'fab fa-linkedin',
-        link: 'https://www.linkedin.com/in/ivo-rojas-415aa244',
+        link: 'https://www.linkedin.com/in/gustavo-pacchi-villarroel/',
       },
       {
-        icon: 'fab fa-github',
-        link: 'https://github.com/ircube',
-      },
-    ],
+        icon: 'fab fa-instagram',
+        link: 'https://www.instagram.com/gusz.pv/?hl=es-la',
+      }
+    ]
   },
+  {
+    name: 'Gabby Torrico',
+    shortName: 'Gabby Torrico',
+    imageSrc: 'assets/img/organizers/gabby-torrico.png',
+    bio: `Gabriela es ing. de sistemas especializada en el area de TI. Tambien disfruta mucho enseñando a los demas, y considera que la mejor forma de aprender es enseñar a alguien mas. Sus hobbies incluyen la natación y los gatos.`,
+    socialLinks: [
+      {
+        icon: 'fab fa-linkedin',
+        link: 'http://www.linkedin.com/in/gabriela-torrico-33925416a',
+      }
+    ]
+  },
+  {
+    name: 'Cesar Serrudo',
+    shortName: 'Cesar Serrudo',
+    imageSrc: 'assets/img/organizers/gustavo-serrudo.jpg',
+    bio: `Cesar es Ingeniero en Informatica con mas de 9 años de experiencias desarrollando software, apasionado de la tecnologia y los videogames actualmente su mayor pasion es ser papa y compartir con su familia y me desarrollo como Senior Software Engineer en tecnologias .Net y desarrollo Web.`,
+    socialLinks: [
+      {
+        icon: 'fab fa-twitter',
+        link: 'https://twitter.com/csardev88',
+      },
+      {
+        icon: 'fab fa-instagram',
+        link: 'https://www.instagram.com/csardev/ Linkedin: www.linkedin.com/in/cesar-gustavo-serrudo-huarachi-674b9a15a',
+      }
+    ]
+  },
+  {
+    name: 'Laura Risueño',
+    shortName: 'Laura Risueño',
+    imageSrc: 'assets/img/organizers/laura-risueno.jpg',
+    bio: `Laura es ingeniera de software con más de 5 años de experiencia ha trabajado en distintas empresas y actualmente trabaja en Assuresoft. Posee habilidades técnicas sólidas y destacadas en resolución de problemas, mentoría, liderazgo y creatividad. Es embajadora de Women Techmakers y colabora activamente en GDG Cochabamba, WTM Cochabamba y Angular Bolivia.`,
+    socialLinks: [
+      {
+        icon: 'fab fa-twitter',
+        link: 'https://twitter.com/LauRisueno',
+      },
+      {
+        icon: 'fab fa-linkedin',
+        link: 'https://www.linkedin.com/in/laurisueno',
+      },
+      {
+        icon: 'fab fa-facebook',
+        link: 'https://www.facebook.com/laurisuenoarancibia',
+      }
+    ]
+  },
+  {
+    name: 'Angel Flores',
+    shortName: 'Angel Flores',
+    imageSrc: 'assets/img/organizers/angel-flores.jpg',
+    bio: `Angel es fullstack developer, actualmente trabaja en Digital Harbor, le apasiona enseñar a los demas, compartiendo su conocimiento y nuevas experiencias tambien le gusta aprender cosas nuevas en el mundo de la tecnologia relacionados con desarrollo web y AI, pertenece a diferentes comunidades como ser Angular Bolivia, GDG y SCESI.`,
+    socialLinks: [
+      {
+        icon: 'fab fa-instagram',
+        link: 'https://www.instagram.com/angel1102flores/',
+      },
+      {
+        icon: 'fab fa-linkedin',
+        link: 'www.linkedin.com/in/angel-flores-lazarte',
+      },
+      {
+        icon: 'fab fa-facebook',
+        link: 'https://www.facebook.com/angelcristian.floreslazarte',
+      }
+    ]
+  },
+  {
+    name: 'Maria Fernanda Nuñez del Prado Barbery',
+    shortName: 'Maria Nuñez',
+    imageSrc: 'assets/img/organizers/maria-nunez.png',
+    bio: `María Fernanda es Ingeniera Industrial y de Sistemas, actualmente trabaja como fullstack en Nice inContact y le gusta que le digan Fer. Le encanta la enseñanza y aprendizaje en diferentes ámbitos como el desarrollo de software, matemáticas y Zumba!.`,
+  },
+  {
+    name: 'Esther Romero Aguilar',
+    shortName: 'Esther Romero',
+    imageSrc: 'assets/img/organizers/esther-romero.jpg',
+    bio: `Esther es estudiante de Ingeniería Informática, miembro activo de GDG Cochabamba y Angular Bolivia, apasionada por las competencias de programación y de voluntariados a comunidades del mundo de la tecnología.`,
+    socialLinks: [
+      {
+        icon: 'fab fa-instagram',
+        link: 'https://www.instagram.com/esther.romeagui',
+      }
+    ]
+  },
+  {
+    name: 'Melvy Rocío Ancieta Alvarado',
+    shortName: 'Melvy Ancieta',
+    imageSrc: 'assets/img/organizers/melvy-ancieta.png',
+    bio: `Melvy es una persona apasionada por el diseño gráfico y la comunicación, actualmente se desempeña como docente universitaria de pre grado y posgrado, disfruta de la lectura y poner su creatividad al servicio de la sociedad.`,
+    socialLinks: [
+      {
+        icon: 'fab fa-facebook',
+        link: 'https://www.facebook.com/melvyrocio.ancieta/ ',
+      }
+    ]
+  },
+  {
+    name: 'Ronald Humberto Chipana Wariste',
+    shortName: 'Ronald Chipana',
+    imageSrc: 'assets/img/mentors/ronald-chipana.jpg',
+    bio: `Es ingeniero de software de profesión y cuenta con experiencia en el área de desarrollo de aplicaciones con herramientas de google, desarrollo de videojuegos para la educación y es docente de programación. Le apasiona la enseñanza de la tecnología razón por la cual  fundó la Academia TechKids donde promueve la educación STEAM desde temprana edad; es una persona muy dinámica y le encanta aprender, razón por la cual forma parte de distintas comunidades como GDG Cochabamba, Women Techmakers Cochabamba y Angular Bolivia.`,
+    socialLinks: [
+      {
+        icon: 'fab fa-linkedin',
+        link: 'https://www.linkedin.com/in/ronald-chipana-w-63382688/',
+      }
+    ]
+  }
 ];
 
 export default organizers;
